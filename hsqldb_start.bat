1e8a9cec1d72511e0905de26ee266d186a2e8d39
Stefan Schindler <stefan.schindler@student.tuwien.ac.at>
2013-01-20 00:21:23 +0100

    added: hsql server.properties changed: hsql start script now uses properties file

--- hsqldb_start.bat
+++ hsqldb_start.bat
@@ -1,3 +1,1 @@
-REM java -cp lib/hsqldb.jar org.hsqldb.Server -database.0 file:db/swazamdb -dbname.0 SWAZAMDB
-REM java -cp lib/hsqldb.jar org.hsqldb.Server -database.0 file:db/swazamserverdb -dbname.0 SWAZAMSERVERDB
-java -cp lib/hsqldb.jar org.hsqldb.Server -database.0 file:db/swazamdb -dbname.0 SWAZAMDB -database.1 file:db/swazamserverdb -dbname.1 SWAZAMSERVERDB
+java -cp lib/hsqldb.jar org.hsqldb.Server --props db/server.properties
